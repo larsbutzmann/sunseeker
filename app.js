381c18b3086e224edb18b847844b17db6215a556
Lars Butzmann <lars.butzmann@gmail.com>
2013-06-25 23:28:50 +0200

    mongodb support

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var express = require("express");
+var mongo = require('mongodb');
 var app = express();
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+});
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
